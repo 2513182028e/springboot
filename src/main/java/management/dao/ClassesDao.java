a14daea2871cbae3b5f76061f6eec320df3b4594
2513182028e <64726241+2513182028e@users.noreply.github.com>
2021-08-15 20:38:27 +0800

    Update ClassesDao.java
    课程信息Dao层

--- src/main/java/management/dao/ClassesDao.java
+++ src/main/java/management/dao/ClassesDao.java
@@ -16,13 +16,14 @@ import java.util.List;
 public interface ClassesDao {
 
 
-    public Classes queryByID(String ID);
+    public Classes queryByID(String ID);            //通过课程编号查询课程信息
 
-    public List<class_teacher> QueryClassesAll();
+    public List<class_teacher> QueryClassesAll();     //查询所有的教师任教关系
+ 
+     public List<class_student> QueryClassesByID(String StudentID);   //通过学生编号查询该学生的选课信息
 
-    public List<class_student> QueryClassesByID(String StudentID);
+    public  List<ViewClass> QueryViewClassAll();                 //查询所以的课程结果（ViewClass类在Class类的基础时加入了TeacherID这个属性）
 
-    public  List<ViewClass> QueryViewClassAll();
-
-    public int countNumber(@Param("TeacherID") String TeacherID,@Param("ClassID") String ClassID);
+    public int countNumber(@Param("TeacherID") String TeacherID,@Param("ClassID") String ClassID);   //从class——student表中查找选了某个老师教的某节课的学生人数，
+    //用来设置ViewClass类中的ClassNumber的属性
 }
