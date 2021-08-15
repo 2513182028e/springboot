b9d6fd05e1262afc7c610f45277deb67df172fe2
2513182028e <64726241+2513182028e@users.noreply.github.com>
2021-08-15 21:05:10 +0800

    Update CastList.java

--- src/main/java/management/utils/CastList.java
+++ src/main/java/management/utils/CastList.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class CastList {
 
                                                                      
-                                                                     //将强行转为List类型的Object的对象，由转为List类型
+                                                                     //将强行转为Object类型的List对象，由转为List类型
     public static <T> List<T> castList(Object obj, Class<T> clazz)
     {
         List<T> result = new ArrayList<T>();
