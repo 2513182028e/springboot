6d13657327d4d108c90fde9a97c519721615a612
2513182028e <64726241+2513182028e@users.noreply.github.com>
2021-08-15 20:52:15 +0800

    Update StudentDao.java

--- src/main/java/management/dao/StudentDao.java
+++ src/main/java/management/dao/StudentDao.java
@@ -14,20 +14,21 @@ import java.util.List;
 @Mapper
 public interface StudentDao {
 
-    public Student queryByName(String username);//1
+    public Student queryByName(String username);//学生编号查询学生信息
 
-    public List<class_student> QueryChosenClasses(@Param("TeacherID") String TeacherID,@Param("ClassID") String ClassID);
+    public List<class_student> QueryChosenClasses(@Param("TeacherID") String TeacherID,@Param("ClassID") String ClassID);     //从class——student表中查找选课关系
 
-    public void deleteClass(@Param("StudentID") String StudentID,@Param("ClassID") String ClassID,@Param("TeacherID") String TeacherID);
+    public void deleteClass(@Param("StudentID") String StudentID,@Param("ClassID") String ClassID,@Param("TeacherID") String TeacherID); //退出已经选择的课
+   
 
-    public void JoinClass(@Param("StudentID") String StudentID,@Param("ClassID") String ClassID,@Param("TeacherID") String TeacherID);//1
-
+    public void JoinClass(@Param("StudentID") String StudentID,@Param("ClassID") String ClassID,@Param("TeacherID") String TeacherID);////选课界面的（由ViewClass和thymeleaf）组成
+     //ViewClass类保护教师ID，课程ID，学生选课
     public class_student selectClass_stu(@Param("StudentID") String StudentID,@Param("ClassID") String ClassID,@Param("TeacherID") String TeacherID);
 
-    public void deleteChosen(@Param("ClassID") String ClassID,@Param("StudentID") String StudentID); //1
+    public void deleteChosen(@Param("ClassID") String ClassID,@Param("StudentID") String StudentID); //退出已经选择的课
 
 
-    public String queryTeacherIDBye(@Param("StudentID") String StudentID,@Param("ClassID") String ClassID);
+    public String queryTeacherIDBye(@Param("StudentID") String StudentID,@Param("ClassID") String ClassID);//从class——student表中查询出教师
 
 
 }
